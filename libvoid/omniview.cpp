1acbcf7d91ff11522e85518e0c772ff91a3e5a3f
Christer Soderlund <christer.soderlund@gmail.com>
2013-09-30 18:13:11 +0200

    Added libvoid.h
    Started work on recursive shadowcasting

---
+++ libvoid/omniview.cpp
@@ -0,0 +1,107 @@
+#include "omniview.h"
+#include "common.h"
+
+using namespace world;
+
+OmniView::OmniView( )
+{
+}
+
+OmniView::~OmniView( )
+{
+}
+
+void OmniView::set_data_source( const char* data, int x, int y )
+{
+    m_data = data;
+    m_data_width = x;
+    m_data_height = y;
+}
+
+void OmniView::update_entity_position( int x, int y )
+{
+    m_entity_x = x;
+    m_entity_y = y;
+    scan();
+}
+
+void OmniView::scan()
+{
+    scan_octant_1( );
+    scan_octant_2( );
+    scan_octant_3( );
+    scan_octant_4( );
+    scan_octant_5( );
+    scan_octant_6( );
+    scan_octant_7( );
+    scan_octant_8( );
+}
+
+// The 8 octant functions
+
+//  Scans is performed row by row, going from the leftmost
+//  cell to the rightmost cell in each row.
+void OmniView::scan_octant_1( )
+{
+    recurse_scan( m_entity_x, m_entity_y, OCTANT_1 );
+}
+
+void OmniView::scan_octant_2( )
+{
+}
+
+void OmniView::scan_octant_3( )
+{
+}
+
+void OmniView::scan_octant_4( )
+{
+}
+
+void OmniView::scan_octant_5( )
+{
+}
+
+//  Scans is performed row by row, going from the leftmost
+//  cell to the rightmost cell in each row.
+void OmniView::scan_octant_6( )
+{
+}
+
+void OmniView::scan_octant_7( )
+{
+}
+
+void OmniView::scan_octant_8( )
+{
+}
+
+void OmniView::recurse_scan( int x, int y, OmniView::OCTANTS octant )
+{
+    int x_start = x;
+    int y_start = y;
+    int x_step = 1;
+    switch ( octant )
+    {
+        case OmniView::OCTANT_1:
+        {
+            // Get characters position
+            // Find scan area
+            // Verify boundaries - going up to the right
+            // Scan a line
+            while ( y_start > 0 ) {
+                --y_start;
+                while ( x_start > 0 ) {
+                    --x_start;
+                    if ( m_data[ x_start * y_start + x_start ] != WALL ) {
+                        ++x_start;
+                    } else {
+                    float angle = x - x_start / y - y_start;
+                    }
+                }
+                x_start
+            }
+            break;
+        }
+    }
+}
---
+++ libvoid/void.h
@@ -0,0 +1,20 @@
+#ifndef _VOID_H_
+#define _VOID_H_
+
+#include "omniview.h"
+namespace world
+{
+
+    class Void
+    {
+    public:
+        Void();
+        ~Void();
+        
+    private:
+        OmniView m_omni;
+    };
+
+} // Namespace world
+
+#endif /* _VOID_H_ */
